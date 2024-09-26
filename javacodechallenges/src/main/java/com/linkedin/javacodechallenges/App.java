8db1c9154d44a629dd665a9e124820ae5cb0961f
Grruom <92161345+Grruom@users.noreply.github.com>
2024-09-26 08:47:16 +0000

    Two versions of complex password checks

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -5,7 +5,31 @@ import java.util.Scanner;
 public class App {
 
     public static boolean isPasswordComplex(String password) {
-        return false;
+
+        return password.length() >=6
+            && password.matches(".*\\d.*")
+            && password.matches(".*[a-z].*")
+            && password.matches(".*[A-Z].*");
+
+/*         if (password.length() < 6) {
+            return false;
+        }
+
+        boolean hasUpperCase = false;
+        boolean hasLowerCase = false;
+        boolean hasNumber = false;
+
+        for (int i = 0; i < password.length() || !hasLowerCase && !hasUpperCase && !hasNumber; i++) {
+            char current = password.charAt(i);
+            if (Character.isDigit(current)) {
+                hasNumber = true;
+            } else if (Character.isLowerCase(current)) {
+                hasLowerCase = true;
+            } else if (Character.isUpperCase(current)) {
+                hasUpperCase = true;
+            }
+        }
+        return hasNumber && hasLowerCase && hasUpperCase; */
     }
 
     public static void main(String[] args) {
