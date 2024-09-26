51776fd5e7dd7e1466d2e7c677179527342f9db6
Grruom <92161345+Grruom@users.noreply.github.com>
2024-09-26 11:07:58 +0000

    Double or nothing logic good and bad

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/DoubleOrNothing.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/DoubleOrNothing.java
@@ -1,6 +1,92 @@
+package com.linkedin.javacodechallenges;
+
+
+import java.util.Scanner;
+import java.util.Random;
+
 public class DoubleOrNothing {
 
+  private static int startPoints = 10;
+  private Scanner scanner = new Scanner(System.in);
+  private Random random = new Random();
+  private int current = startPoints;
+  private boolean isStillPlaying = true;
+
   public void playGame() {
-    // TODO: Implement method
+
+    printWelcome();
+    askUserToContinue();
+
+    while (this.isStillPlaying) {
+      printTryToDouble();
+
+      if (shouldDouble()) {
+        doubleCurrent();
+        printCongrats();
+        askUserToContinue();
+      } else {
+        printLost();
+        return;
+      }
+    } printWalkawayMessage();
   }
+
+  private void printWelcome () {
+    System.out.println("Welcome to a game of double!");    
+  }
+
+  private void printTryToDouble () {
+    System.out.println("Lets try to double it!");
+  }
+
+  private void printCongrats () {
+    System.out.println("Congratulations! You now have: " + this.current);
+  }
+
+  private void printLost () {
+    System.out.println("Uh oh. You've lost. No points for you");
+  }
+
+  private void printWalkawayMessage() {
+    System.out.println("Congratulasion! You finished with: " + this.current);
+  }
+
+  private boolean shouldDouble () {
+    return this.random.nextInt(2) == 0;
+  }
+
+  private void doubleCurrent () {
+    this.current = this.current * 2;
+  }
+
+  protected void askUserToContinue () {
+    System.out.println("Would you like to continue? ");
+    this.isStillPlaying = this.scanner.nextLine().toUpperCase().equals("YES");
+  }
+
+  protected boolean getIsStillPlaying() {
+    return this.isStillPlaying;
+  }
+
+
+/*     Scanner scanner = new Scanner(System.in);
+    Random random = new Random();
+
+    System.out.println(" --Welcome to Double or Nothing!-- ");
+    System.out.println("Enter you bet amount: ");
+    double bet = scanner.nextDouble();
+
+    System.out.println("Flipping the coin...");
+    boolean win = random.nextBoolean();
+
+    if (win) {
+      bet *= 2;
+      System.out.println("Congratulations! You doubble your money!");
+      System.out.println("Your new balance is: " + bet);
+    } else {
+      System.out.println("Sorry, you didnt win this time");
+    }
+
+    scanner.close(); */
+
 }
--- javacodechallenges/src/test/java/com/linkedin/javacodechallenges/AppTest.java
+++ javacodechallenges/src/test/java/com/linkedin/javacodechallenges/AppTest.java
@@ -1,20 +1,44 @@
 package com.linkedin.javacodechallenges;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class AppTest {
+    private final ByteArrayOutputStream gamePrintOut = new ByteArrayOutputStream();
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(gamePrintOut));
+    }
+
+    @After
+    public void restoreStreams() {
+        System.setOut(System.out);
+    }
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void askUserToContinue_YES() {
+        String input = "yes";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        DoubleOrNothing game = new DoubleOrNothing();
+        assertTrue(game.getIsStillPlaying());
+
+        game.askUserToContinue();
+        assertTrue(game.getIsStillPlaying());
+        assertEquals("Would you like to continue? ", gamePrintOut.toString());
     }
 }
